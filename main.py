30ace8d925fd34cae6d085a5aa488b2a82537fdd
nana <nana.gaming0120@gmail.com>
2024-09-04 12:42:53 +0800

    apply qss for styling

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from src.mainwindow import MainWindow
 app = QApplication()
 
 window = MainWindow()
+with open("src/styles.qss", "r") as _styles:
+    app.setStyleSheet(_styles.read())
 window.show()
 
 app.exec()
